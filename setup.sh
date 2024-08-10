7590a058a53d72297a10aa55a318b3385991747a
trungdq1 <121534595+trungdq1@users.noreply.github.com>
2024-08-10 16:10:52 +0700

    setup.sh

--- setup.sh
+++ setup.sh
@@ -83,7 +83,7 @@ IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 echo "Internal IP = ${IP4}. External sub for IP6 = ${IP6}"
 
 FIRST_PORT=22000
-LAST_PORT=22700
+LAST_PORT=22999
 
 gen_data >$WORKDIR/data.txt
 gen_ifconfig >$WORKDIR/boot_ifconfig.sh
